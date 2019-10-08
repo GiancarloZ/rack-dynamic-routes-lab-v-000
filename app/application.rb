97528a28aee39c3930a95ad165c3c19ad74eb38e
Giancarlo Zabaleta <gzabaleta48@gmail.com>
2019-10-08 22:18:52 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -5,11 +5,8 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
  
-    if req.path==""
- 
-    item = req.path.split("/songs/").last 
-    song = @@songs.find{|s| s.title == song_title}
-
+    if req.path=="/items/"
+      resp.write "You requested the songs"
     else
       resp.write "Route not found"
       resp.status = 404
