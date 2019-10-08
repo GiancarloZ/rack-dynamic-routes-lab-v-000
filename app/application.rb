0980a41e21a2c3e283293a1cf53d3eade133d199
Giancarlo Zabaleta <gzabaleta48@gmail.com>
2019-10-08 22:22:23 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -5,9 +5,15 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
  
-    if req.path.match(/items\/)
-      @@items.include?()  
-      resp.write "You requested the songs"
+    if req.path.match(/items\/.+/)
+      item_name = req.path.split("/items/").last
+      item = @@items.detect { |i| i.name == item_name }
+      if item
+        resp.write item.price
+      else
+        resp.write "Item not found"
+        resp.status = 400
+      end
     else
       resp.write "Route not found"
       resp.status = 404
